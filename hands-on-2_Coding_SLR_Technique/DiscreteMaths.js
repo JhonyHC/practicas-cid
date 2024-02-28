ce22cb1f26971fa821d8cba768ce441a956e2e28
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2024-02-28 07:59:03 -0600

    Add DiscreteMaths Class

---
+++ hands-on-2_Coding_SLR_Technique/DiscreteMaths.js
@@ -0,0 +1,20 @@
+export default class DiscreteMaths {
+  constructor() {}
+
+  static sumatory(numbers) {
+    return numbers.reduce((sum, number) => (sum += number), 0);
+  }
+
+  static arraySquare(numbers) {
+    return numbers.map((number) => number * number);
+  }
+
+  static multiplyArrays(arr1, arr2) {
+    if (arr1.length !== arr2.length) return null;
+    return arr1.map((item, index) => item * arr2[index]);
+  }
+
+  static randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+}
