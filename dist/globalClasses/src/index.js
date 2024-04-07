e4c8bf74f66325159e3a9f28962b45903bb114d0
JhonyHC <jonhatan.higuera5180@alumnos.udg.mx>
2024-04-07 17:24:01 -0600

    Update gitignore and add remove dist folder

--- dist/globalClasses/src/index.js
+++
@@ -1,5 +0,0 @@
-import DataSet from "./DataSet.js";
-import DiscreteMaths from "./DiscreteMaths.js";
-import LinearRegression from "./LinearRegression.js";
-export { DataSet, DiscreteMaths, LinearRegression };
-//# sourceMappingURL=index.js.map
